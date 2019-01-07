4701425f984cdca712e39a474d91d6e87b0a7748
farseer <457579870@qq.com>
2019-01-07 17:12:04 +0800

    第一个测试版本

---
+++ demo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Farseer
+ * Date: 2019/1/7
+ * Time: 5:08 PM
+ */
+
+require_once "./vendor/autoload.php";
+
+use Zhuangbi\Test\Tools;
+
+Tools::hello();
---
+++ src/Zhuangbi/Test/Tools.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Farseer
+ * Date: 2019/1/7
+ * Time: 4:59 PM
+ */
+
+namespace Zhuangbi\Test;
+
+
+class Tools
+{
+    public function __construct()
+    {
+    }
+
+    public static function hello()
+    {
+        echo "hello";
+    }
+}
